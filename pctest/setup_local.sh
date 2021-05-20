226f0c2663c5f95d38b8bcbe80d343f8ce1c53cb
Richard Brooks <richardbrooks2000@gmail.com>
2021-05-20 15:17:03 +0000

    script to create an admin account, deploy the on-chain program and create and initialize a mapping account for testing

---
+++ pctest/setup_local.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+PYTH=./pyth
+SOLANA=../../solana/target/release/solana
+SOL_OPT="-u localhost --commitment finalized"
+RPC="-r localhost"
+FINAL="-c finalized"
+
+check()
+{
+  CMD=$1
+  echo "$CMD"
+  eval $CMD
+  RC=$?
+  if [ $RC -ne 0 ]; then
+    echo "unexpected error executing rc= $RC"
+    exit 1
+  fi
+}
+
+setup_admin()
+{
+  DIR=$1
+  OPT="-k $DIR $RPC $FINAL"
+  chmod 0700 $DIR
+
+  # create publisher key and fund it
+  check "$PYTH init_key $OPT"
+  check "$SOLANA airdrop 10 -k $DIR/publish_key_pair.json $SOL_OPT"
+
+  # create program key
+  check "$PYTH init_program $OPT"
+
+  # setup program key and deploy
+  check "$SOLANA program deploy ../target/oracle.so -k $DIR/publish_key_pair.json --program-id $DIR/program_key_pair.json $SOL_OPT"
+
+  # setup mapping account
+  check "$PYTH init_mapping $OPT"
+}
+
+# create key_store directory and initialize key, program and mapping
+# accounts assuming you have a local build of solana and are running
+# both a solana-validator and solana-faucet on localhost
+# run from build/ directory
+KDIR=$1
+if [ -z "$KDIR" ] ; then
+  echo "set_local.sh <key_store_directory>"
+  exit 1
+fi
+setup_admin $KDIR
