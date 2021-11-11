c5ad7643a619802f74929e5a63ca031c9adc44b1
Tony Ricciardi <4278073+tony-ricciardi@users.noreply.github.com>
2021-11-10 19:11:51 -0500

    `build.sh`: - Change default dir to `build` (pwd-relative), for usage in other projects. - Check for `CMakeLists.txt` before `rm -rf $BUILD_DIR`. - Document example usages. - Use `/usr/bin/env bash` for Mac users with homebrew bash.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# release build:
+#   ~/pyth-client$ ./scripts/build.sh [build]
+#
+# debug build:
+#   ~/pyth-client$ ./scripts/build.sh debug
+#
+# build other project:
+#   ~/pyth-client$ ./scripts/build.sh ../serum-pyth/build
+#   ~/serum-pyth$ ../pyth-client/scripts/build.sh
+#
 
 set -eu
 
@@ -7,10 +18,7 @@ then
   BUILD_DIR="$1"
   shift
 else
-  THIS_DIR="$( dirname "${BASH_SOURCE[0]}" )"
-  THIS_DIR="$( cd "${THIS_DIR}" && pwd )"
-  ROOT_DIR="$( dirname "${THIS_DIR}" )"
-  BUILD_DIR="${ROOT_DIR}/build"
+  BUILD_DIR="build"
 fi
 
 ROOT_DIR="$( mkdir -p "${BUILD_DIR}" && cd "${BUILD_DIR}/.." && pwd )"
@@ -19,6 +27,13 @@ BUILD_DIR="$( realpath --relative-to="${ROOT_DIR}" "${BUILD_DIR}" )"
 if [[ ! -v CMAKE_ARGS && $BUILD_DIR == *debug* ]]
 then
   CMAKE_ARGS=( "-DCMAKE_BUILD_TYPE=Debug" )
+fi
+
+# Check before rm -rf $BUILD_DIR
+if [[ ! -f "${ROOT_DIR}/CMakeLists.txt" ]]
+then
+  >&2 echo "Not a cmake project dir: ${ROOT_DIR}"
+  exit 1
 fi
 
 set -x
