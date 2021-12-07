f740d1dc75b4db8c149eda5b9f3f311d7780490b
James Hunsaker <hunsaker@spacetime.org>
2021-12-07 13:48:06 -0600

    test multiple products

--- pyth/tests/test_get_product.py
+++ pyth/tests/test_get_product.py
@@ -2,6 +2,9 @@ import asyncio
 import json
 import websockets
 
+from pyth.tests.conftest import PRODUCTS
+
+
 def test_get_product_list(solana_test_validator, pythd, pyth_init_price):
 
     data = {
@@ -10,6 +13,7 @@ def test_get_product_list(solana_test_validator, pythd, pyth_init_price):
       'method': 'get_product_list',
     }
     data = json.dumps(data)
+
     async def f():
         async with websockets.connect('ws://localhost:8910/') as ws:
             await ws.send(data)
@@ -18,20 +22,13 @@ def test_get_product_list(solana_test_validator, pythd, pyth_init_price):
     result = asyncio.run(f())
     result = json.loads(result)
     result = result['result']
-    result = result[0]
+    result = result[2]
     del result['account']  # TODO
     expected_result = {
-        'attr_dict': {
-            'symbol': 'LTC/USD',
-            'asset_type': 'Crypto',
-            'country': 'US',
-            'quote_currency': 'USD',
-            'tenor': 'Spot',
-            'jlqd_symbol': 'LTCUSD',
-        },
+        'attr_dict': PRODUCTS['LTC'],
         'price': [
             {
-                'account': pyth_init_price,
+                'account': pyth_init_price['LTC'],
                 'price_exponent': -5,
                 'price_type': 'price',
             },
