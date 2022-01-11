9871a74f6a6eb7884e30fa55ede47a3bc9165c5f
Kevin J Bowers <kbowers@jumptrading.com>
2022-01-11 09:37:08 -0600

    Generic utilities for efficient code usable on and off chain with standalone off chain test suite

---
+++ program/src/oracle/util/test_sar.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "util.h"
+
+int
+main( int     argc,
+      char ** argv ) {
+  (void)argc; (void)argv;
+
+  prng_t _prng[1];
+  prng_t * prng = prng_join( prng_new( _prng, (uint32_t)0, (uint64_t)0 ) );
+
+  /* FIXME: EXPLICT COVERAGE OF EDGE CASES (PROBABLY STATICALLY FULLY
+     SAMPLED ALREADY THOUGH FOR 8 AND 16 BIT TYPES) */
+
+  int ctr = 0;                                                                                                                      
+  for( int i=0; i<1000000000; i++ ) {
+    if( !ctr ) { printf( "Completed %i iterations\n", i ); ctr = 10000000; }
+    ctr--;
+
+    /* These tests assume the unit test platform has arithmetic right
+       shift for signed integers (and the diagnostic printfs assume that
+       long is at least a w-bit wide type) */
+
+#   define TEST_SAR(w) do {                                                                                  \
+      int##w##_t x   = (int##w##_t)prng_uint##w( prng );                                                     \
+      int        n   = (int)( prng_uint32( prng ) & (uint32_t)(w-1) );                                       \
+      int##w##_t val = sar_int##w( x, n );                                                                   \
+      int##w##_t ref = (int##w##_t)(x >> n);                                                                 \
+      if( val!=ref ) {                                                                                       \
+        printf( "FAIL (iter %i op %i x %li n %i val %li ref %li)", i, w, (long)x, n, (long)val, (long)ref ); \
+        return 1;                                                                                            \
+      }                                                                                                      \
+    } while(0)
+
+    TEST_SAR(8);
+    TEST_SAR(16);
+    TEST_SAR(32);
+    TEST_SAR(64);
+#   undef TEST_SAR
+
+  }
+
+  prng_delete( prng_leave( prng ) );
+
+  printf( "pass\n" );
+
+  return 0;
+}
+
