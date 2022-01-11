a911ded09c9928767598652ba368c5a32fa6f13d
Kevin J Bowers <kbowers@jumptrading.com>
2022-01-11 11:46:49 -0600

    Solana uses its own custom replacement for stdint

---
+++ program/src/oracle/util/compat_stdint.h
@@ -0,0 +1,13 @@
+#ifndef _pyth_oracle_util_compat_stdint_h_
+#define _pyth_oracle_util_compat_stdint_h_
+
+/* solana uses its own definitions for stdint types and that can cause
+   problems with compilation */
+
+#ifdef __bpf__
+#include <solana_sdk.h>
+#else
+#include <stdint.h>
+#endif
+
+#endif /* _pyth_oracle_util_compat_stdint_h_ */
