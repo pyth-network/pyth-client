0de2871383016a46827cfb44246ad4501cda9bde
guibescos <gbescos@stanford.edu>
2022-07-19 00:42:44 +0000

    Some cleanup

--- program/rust/src/error.rs
+++ program/rust/src/error.rs
@@ -1,36 +1,21 @@
 //! Error types
 
-use num_derive::FromPrimitive;
 use solana_program::{
-    decode_error::DecodeError,
-    msg,
-    program_error::{PrintProgramError, ProgramError},
+    program_error::{ProgramError},
 };
 use thiserror::Error;
 
 
 /// Errors that may be returned by the oracle program
-#[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
+#[derive(Clone, Debug, Eq, Error, PartialEq)]
 pub enum OracleError {
-    /// Invalid instruction passed to program
+    /// Generic catch all error
     #[error("Generic")]
-    Generic = 600, //
-}
-
-impl PrintProgramError for OracleError {
-    fn print<E>(&self) {
-        msg!("ORACLE-ERROR: {}", &self.to_string());
-    }
+    Generic = 600,
 }
 
 impl From<OracleError> for ProgramError {
     fn from(e: OracleError) -> Self {
         ProgramError::Custom(e as u32)
-    }
-}
-
-impl<T> DecodeError<T> for OracleError {
-    fn type_of() -> &'static str {
-        "Oracle Error"
     }
 }
--- program/rust/src/lib.rs
+++ program/rust/src/lib.rs
@@ -3,12 +3,16 @@ mod c_oracle_header;
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::entrypoint::{deserialize, ProgramResult};
 use solana_program::msg;
-use solana_program::{account_info::AccountInfo, pubkey::Pubkey};
+use solana_program::{account_info::AccountInfo, pubkey::Pubkey, program_error::PrintProgramError};
 
 mod bindings;
 mod error;
 use bindings::{
-    cmd_hdr, cmd_upd_price, command_t_e_cmd_upd_price, command_t_e_cmd_upd_price_no_fail_on_error,
+    cmd_hdr, cmd_upd_price, command_t_e_cmd_add_mapping, command_t_e_cmd_add_price,
+    command_t_e_cmd_add_product, command_t_e_cmd_add_publisher, command_t_e_cmd_del_publisher,
+    command_t_e_cmd_init_mapping, command_t_e_cmd_init_price, command_t_e_cmd_set_min_pub,
+    command_t_e_cmd_upd_price, command_t_e_cmd_upd_price_no_fail_on_error, command_t_e_cmd_agg_price,
+    command_t_e_cmd_upd_product
 };
 use error::OracleError;
 
@@ -69,7 +73,48 @@ pub fn pre_process(
                 instruction.status_
             );
         }
-        _ => {}
+        command_t_e_cmd_agg_price => {
+            let instruction: cmd_upd_price =
+            cmd_upd_price::try_from_slice(&instruction_data).unwrap();
+            msg!(
+                "Update price: price={:}, conf={:}, status={:}",
+                instruction.price_,
+                instruction.conf_,
+                instruction.status_
+            );
+        }
+        command_t_e_cmd_add_mapping => {
+            msg!("Add mapping");
+        }
+        command_t_e_cmd_add_price => {
+            msg!("Add price");
+        }
+        command_t_e_cmd_add_product => {
+            msg!("Add product")
+        }
+        command_t_e_cmd_add_publisher => {
+            msg!("Add publisher")
+        }
+        command_t_e_cmd_del_publisher => {
+            msg!("Delete publisher")
+        }
+        command_t_e_cmd_init_price => {
+            msg!("Initialize price")
+        }
+        command_t_e_cmd_init_mapping => {
+            msg!("Initialize mapping account");
+        }
+
+        command_t_e_cmd_set_min_pub =>{
+            msg!("Set minimum number of publishers");
+        }
+        command_t_e_cmd_upd_product =>{
+            msg!("Update product");
+        }
+        _ => {
+            msg!("Unrecognized instruction");
+            return Err(OracleError::Generic.into());
+        }
     }
     Ok(())
 
