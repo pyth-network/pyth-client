38c0210df0ccbcee1b9678a76ed222e006830b83
Mark Jabbour <markmarjabbour@gmail.com>
2022-07-20 14:16:37 +0000

    made successful aggregate update greater than 15

--- program/c/src/oracle/oracle.h
+++ program/c/src/oracle/oracle.h
@@ -11,7 +11,7 @@ extern "C" {
 //values 0-14 are defined in solana_sdk.h
 //used consts instead of define because bingen always turns
 // defines to u32 (even with ULL suffix)
-const uint64_t SUCCESSFULLY_UPDATED_AGGREGATE = 15ULL;
+const uint64_t SUCCESSFULLY_UPDATED_AGGREGATE = 1000ULL;
 
 // magic number at head of account
 #define PC_MAGIC 0xa1b2c3d4
