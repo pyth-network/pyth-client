d37adc9d3a58c0de7e0b5831e13380f609c31285
Mark Jabbour <markmarjabbour@gmail.com>
2022-07-20 14:19:01 +0000

    timeless coments

--- program/c/src/oracle/oracle.h
+++ program/c/src/oracle/oracle.h
@@ -6,7 +6,6 @@
 extern "C" {
 #endif
 
-
 //A return value indicating that the aggregate price was updated
 //this triggers SMA trackers to update
 //values 0-14 are defined in solana_sdk.h (v1.10.31 )
