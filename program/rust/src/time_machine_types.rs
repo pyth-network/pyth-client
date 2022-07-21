9b3a9ce4fb337cad3ec1a8849a43c6056f370cb8
majabbour <59592028+majabbour@users.noreply.github.com>
2022-07-21 15:42:52 -0500

    Syncing account sizes between c and rust (#197)
    * defined size in oracle.h and asserted it in rust
    
    * added docstring for the test
    
    * updated the doc
    
    * updated the error message
    
    * Update comment in program/c/src/oracle/oracle.h
    
    Co-authored-by: Tom Pointon <tom@teepeestudios.net>
    
    * updated time machine size name
    
    Co-authored-by: Tom Pointon <tom@teepeestudios.net>

---
+++ program/rust/src/time_machine_types.rs
@@ -0,0 +1,20 @@
+use super::c_oracle_header;
+#[derive(Debug, Clone)]
+#[repr(C)]
+/// this wraps multiple SMA and tick trackers, and includes all the state 
+/// used by the time machine 
+pub struct TimeMachineWrapper{
+    //Place holder with the size of the fields I am planning to add
+    place_holder: [u8;1864],
+}
+
+#[test]
+///test that the size defined in C matches that
+///defined in Rust
+fn c_time_machine_size_is_correct() {
+    assert_eq!(
+        ::std::mem::size_of::<TimeMachineWrapper>(),
+        c_oracle_header::TIME_MACHINE_STRUCT_SIZE.try_into().unwrap(),
+        "expected TIME_MACHINE_STRUCT_SIZE in oracle.h to the same as the size of TimeMachineWrapper"
+    );
+}
