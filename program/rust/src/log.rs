b426d38857c14b18a03c528697829c038d710804
Guillermo Bescos <guibescos>
2022-07-22 19:14:05 +0000

    Format and reorder

--- program/rust/src/log.rs
+++ program/rust/src/log.rs
@@ -1,16 +1,9 @@
-use crate::c_oracle_header::{
-    cmd_hdr, cmd_upd_price, command_t_e_cmd_add_mapping, command_t_e_cmd_add_price,
-    command_t_e_cmd_add_product, command_t_e_cmd_add_publisher, command_t_e_cmd_agg_price,
-    command_t_e_cmd_del_publisher, command_t_e_cmd_init_mapping, command_t_e_cmd_init_price,
-    command_t_e_cmd_set_min_pub, command_t_e_cmd_upd_price,
-    command_t_e_cmd_upd_price_no_fail_on_error, command_t_e_cmd_upd_product, pc_price_info,
-    PRICE_T_AGGREGATE_OFFSET, SUCCESSFULLY_UPDATED_AGGREGATE,
-};
+use crate::c_oracle_header::*;
 use crate::error::OracleError;
 use borsh::BorshDeserialize;
+use solana_program::account_info::AccountInfo;
 use solana_program::entrypoint::ProgramResult;
 use solana_program::msg;
-use solana_program::{account_info::AccountInfo};
 use std::mem::size_of;
 
 pub fn pre_log(instruction_data: &[u8]) -> ProgramResult {
@@ -22,15 +15,6 @@ pub fn pre_log(instruction_data: &[u8]) -> ProgramResult {
         .try_into()
         .map_err(|_| OracleError::Generic)?;
     match instruction_id {
-        command_t_e_cmd_upd_price_no_fail_on_error => {
-            let instruction: cmd_upd_price = cmd_upd_price::try_from_slice(instruction_data)?;
-            msg!(
-                "Update price no fail on error: price={:}, conf={:}, status={:}",
-                instruction.price_,
-                instruction.conf_,
-                instruction.status_
-            );
-        }
         command_t_e_cmd_upd_price => {
             let instruction: cmd_upd_price = cmd_upd_price::try_from_slice(instruction_data)?;
             msg!(
@@ -49,6 +33,16 @@ pub fn pre_log(instruction_data: &[u8]) -> ProgramResult {
                 instruction.status_
             );
         }
+        command_t_e_cmd_upd_price_no_fail_on_error => {
+            let instruction: cmd_upd_price = cmd_upd_price::try_from_slice(instruction_data)?;
+            msg!(
+                "Update price no fail on error: price={:}, conf={:}, status={:}",
+                instruction.price_,
+                instruction.conf_,
+                instruction.status_
+            );
+        }
+
         command_t_e_cmd_add_mapping => {
             msg!("Add mapping");
         }
