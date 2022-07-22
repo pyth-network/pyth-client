28f34089c15b52465ec38e942f9a9dc99782ecaa
Guillermo Bescos <guibescos>
2022-07-22 19:01:30 +0000

    Fix inconsistent naming

--- program/rust/src/price_t_offsets.h
+++ program/rust/src/price_t_offsets.h
@@ -1,13 +1,13 @@
-//The constans below are used by the rust oracle to efficiently access members in price_t
+//The constants below are used by the rust oracle to efficiently access members in price_t
 //price_t is a big struct, attemting to deserialize all of it directly in borsh causes stack
-//issues on Solana, it is also more efficient to only access the members we need
+//issues on Solana. It is also more efficient to only access the members we need
 #include "../../c/src/oracle/oracle.h"
 #include <stddef.h>
 const size_t PRICE_T_EXPO_OFFSET = offsetof(struct pc_price, expo_);
 const size_t PRICE_T_TIMESTAMP_OFFSET = offsetof(struct pc_price, timestamp_);
-const size_t PRICE_T_CONF_OFFSET = offsetof(struct pc_price, agg_) + offsetof(struct pc_price_info, conf_);
-const size_t PRICE_T_AGGREGATE_OFFSET = offsetof(struct pc_price, agg_) + offsetof(struct pc_price_info, price_);
-const size_t PRICE_T_AGGREGATE_STATUS = offsetof(struct pc_price, agg_) + offsetof(struct pc_price_info, status_);
+const size_t PRICE_T_AGGREGATE_CONF_OFFSET = offsetof(struct pc_price, agg_) + offsetof(struct pc_price_info, conf_);
+const size_t PRICE_T_AGGREGATE_PRICE_OFFSET = offsetof(struct pc_price, agg_) + offsetof(struct pc_price_info, price_);
+const size_t PRICE_T_AGGREGATE_STATUS_OFFSET = offsetof(struct pc_price, agg_) + offsetof(struct pc_price_info, status_);
 const size_t PRICE_T_PREV_TIMESTAMP_OFFSET = offsetof(struct pc_price, prev_timestamp_);
 const size_t PRICE_T_PREV_CONF_OFFSET = offsetof(struct pc_price, prev_conf_);
 const size_t PRICE_T_PREV_AGGREGATE_OFFSET = offsetof(struct pc_price, prev_price_);
