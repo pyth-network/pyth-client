d522ff4c813b99b77ab7351e63378c65c298178f
majabbour <59592028+majabbour@users.noreply.github.com>
2022-07-22 12:04:38 -0500

    Borsch traits using bindgen (#196)
    * changed c return value of update price to indicate aggregation (#190)
    
    * changed c return value of update price to indicate aggregation
    
    * forgot to add bindings
    
    * removed the need for casting
    
    * fixed update_no_fail
    
    * removed unnecessary docker packages
    
    * timeless coments
    
    * added comment clarifying that 0 is success
    
    * a convinient way to derive traits
    
    * silenced unused bindings warnings
    
    * removed dead code
    
    * added a comment explaining the structure
    
    * changed importing style
    
    * called fmt on bindings output
    
    * added register_traits method
    
    * removed unnecessary  match
    
    * fixed formatting
    
    * made it easier to add borsch to more types by not using vectors

--- program/rust/build.rs
+++ program/rust/build.rs
@@ -1,3 +1,26 @@
+mod build_utils;
+use bindgen::Builder;
+use std::vec::Vec;
+
 fn main() {
     println!("cargo:rustc-link-search=../c/target");
+
+    let borsh_derives = ["BorshSerialize".to_string(), "BorshDeserialize".to_string()];
+
+    //make a parser and to it type, traits pairs
+    let mut parser = build_utils::DeriveAdderParserCallback::new();
+    parser.register_traits("cmd_hdr", borsh_derives.to_vec());
+    parser.register_traits("pc_acc", borsh_derives.to_vec());
+
+
+    //generate and write bindings
+    let bindings = Builder::default()
+        .header("./src/bindings.h")
+        .parse_callbacks(Box::new(parser))
+        .rustfmt_bindings(true)
+        .generate()
+        .expect("Unable to generate bindings");
+    bindings
+        .write_to_file("./bindings.rs")
+        .expect("Couldn't write bindings!");
 }
---
+++ program/rust/build_utils.rs
@@ -0,0 +1,35 @@
+use bindgen::callbacks::ParseCallbacks;
+use std::collections::HashMap;
+use std::panic::UnwindSafe;
+///This type stores a hashmap from structnames
+///to vectors of trait names, and ensures
+///that the traits of each struct are added to its
+///definition when an instance of this struct
+///is provided as a ParseCallback for bindgen
+#[derive(Debug, Default)]
+pub struct DeriveAdderParserCallback<'a> {
+    pub types_to_traits: HashMap<&'a str, Vec<String>>,
+}
+
+impl<'a> DeriveAdderParserCallback<'a> {
+    ///create a parser that does not add any traits
+    pub fn new() -> Self {
+        Default::default()
+    }
+    //add pairs of types and their desired traits
+    pub fn register_traits(&mut self, type_name: &'a str, traits: Vec<String>) {
+        self.types_to_traits.insert(&type_name, traits);
+    }
+}
+
+//this is required to implement the callback trait
+impl UnwindSafe for DeriveAdderParserCallback<'_> {}
+
+impl ParseCallbacks for DeriveAdderParserCallback<'_> {
+    fn add_derives(&self, _name: &str) -> Vec<String> {
+        self.types_to_traits
+            .get(_name)
+            .unwrap_or(&Vec::<String>::new())
+            .to_vec()
+    }
+}
