5757d22b71f96a0a9237a55bf417293e12382910
Mark Jabbour <markmarjabbour@gmail.com>
2022-07-26 15:41:40 +0000

    moved dispatching to processor

--- program/rust/src/lib.rs
+++ program/rust/src/lib.rs
@@ -3,17 +3,14 @@ mod error;
 mod log;
 mod rust_oracle;
 mod time_machine_types;
+mod processor;
 
-use crate::c_oracle_header::{
-    cmd_hdr, command_t_e_cmd_agg_price, command_t_e_cmd_upd_account_version,
-    command_t_e_cmd_upd_price, command_t_e_cmd_upd_price_no_fail_on_error, PC_VERSION,
-    SUCCESSFULLY_UPDATED_AGGREGATE,
-};
+use crate::c_oracle_header::SUCCESSFULLY_UPDATED_AGGREGATE;
 use crate::log::{post_log, pre_log};
-use crate::rust_oracle::{update_price, update_version};
-use ::std::mem::size_of;
-use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{custom_heap_default, custom_panic_default, entrypoint::deserialize};
+use processor::process_instruction;
+use solana_program::pubkey::Pubkey;
+use solana_program::sysvar::slot_history::AccountInfo;
 
 //Below is a high lever description of the rust/c setup.
 
@@ -54,27 +51,12 @@ pub extern "C" fn entrypoint(input: *mut u8) -> u64 {
         _ => {}
     }
 
-    let cmd_hdr_size = size_of::<cmd_hdr>();
-    let cmd_data = cmd_hdr::try_from_slice(&instruction_data[..cmd_hdr_size]).unwrap();
-
-    if cmd_data.ver_ != PC_VERSION {
-        //FIXME: I am not sure what's best to do here (this is copied from C)
-        // it seems to me like we should not break when version numbers change
-        //instead we should log a message that asks users to call update_version
-        panic!("incorrect version numbers");
-    }
-
-    let c_ret_val = match cmd_data.cmd_ as u32 {
-        command_t_e_cmd_upd_price
-        | command_t_e_cmd_upd_price_no_fail_on_error
-        | command_t_e_cmd_agg_price => {
-            update_price(program_id, &accounts, &instruction_data, input)
-        }
-        command_t_e_cmd_upd_account_version => {
-            update_version(program_id, &accounts, &instruction_data)
-        }
-        _ => unsafe { return c_entrypoint(input) },
-    };
+    let c_ret_val = process_instruction(
+        program_id,
+        &accounts, 
+        instruction_data,  
+        input
+    );
 
     match post_log(c_ret_val, &accounts) {
         Err(error) => return error.into(),
---
+++ program/rust/src/processor.rs
@@ -0,0 +1,41 @@
+use crate::c_oracle_header::{
+    cmd_hdr, command_t_e_cmd_agg_price, command_t_e_cmd_upd_account_version,
+    command_t_e_cmd_upd_price, command_t_e_cmd_upd_price_no_fail_on_error, PC_VERSION
+};
+use crate::c_entrypoint;
+use crate::rust_oracle::{update_price, update_version};
+use ::std::mem::size_of;
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::pubkey::Pubkey;
+use solana_program::sysvar::slot_history::AccountInfo;
+
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &Vec<AccountInfo>, 
+    instruction_data: &[u8],  
+    input: *mut u8
+) -> u64 {
+
+    let cmd_hdr_size = size_of::<cmd_hdr>();
+    let cmd_data = cmd_hdr::try_from_slice(&instruction_data[..cmd_hdr_size]).unwrap();
+
+    if cmd_data.ver_ != PC_VERSION {
+        //FIXME: I am not sure what's best to do here (this is copied from C)
+        // it seems to me like we should not break when version numbers change
+        //instead we should log a message that asks users to call update_version
+        panic!("incorrect version numbers");
+    }
+
+    match cmd_data.cmd_ as u32 {
+        command_t_e_cmd_upd_price
+        | command_t_e_cmd_upd_price_no_fail_on_error
+        | command_t_e_cmd_agg_price => {
+            update_price(program_id, &accounts, &instruction_data, input)
+        }
+        command_t_e_cmd_upd_account_version => {
+            update_version(program_id, &accounts, &instruction_data)
+        }
+        _ => unsafe { return c_entrypoint(input) },
+    }
+}
