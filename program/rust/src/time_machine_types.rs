b7752a6f43d2b944753a49f6413602953af0c973
Mark Jabbour <markmarjabbour@gmail.com>
2022-07-27 21:44:46 +0000

    defined the interface for TimeMachine

--- program/rust/src/time_machine_types.rs
+++ program/rust/src/time_machine_types.rs
@@ -18,6 +18,24 @@ impl Default for TimeMachineWrapper {
     }
 }
 
+impl TimeMachineWrapper{
+    /// Add a price to the trackers
+    /// automatically calls the method below if prev_time=0
+    fn add_price(&mut self, current_time: u64, prev_time: u64, current_price: u64, prev_price: u64,  current_conf: u64, prev_conf: u64) {
+        panic!("implement me!");
+    }
+    /// Add a price to the tracker when the tracker is first initialized or
+    /// price account is new
+    fn first_add_price(&mut self, current_time: u64, current_price: u64,  current_conf: u64) {
+        panic!("implement me!");
+    }
+    /// gets the given entry from the tracker with the
+    fn get_entry(&self, current_time: u64, entry_time: u64,granuality: u64, is_twap: bool) -> (u64, u8) {
+        panic!("implement me!");
+        (0, 0)
+    }
+}
+
 #[test]
 ///test that the size defined in C matches that
 ///defined in Rust
