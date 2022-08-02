51291e4ead7cad4321785b9ff033f3edd181fbd7
Guillermo Bescos <gbescos@stanford.edu>
2022-08-02 23:59:53 +0000

    Restore allow

--- program/rust/src/c_oracle_header.rs
+++ program/rust/src/c_oracle_header.rs
@@ -1,4 +1,6 @@
 #![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+//we do not use all the variables in oracle.h, so this helps with the warnings
 #![allow(dead_code)]
 //All the custom trait imports should go here
 use borsh::{
