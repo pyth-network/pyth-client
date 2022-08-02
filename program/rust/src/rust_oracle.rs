17462d2834efb3b90a914860fe771e95c6fea7df
Jayant Krishnamurthy <jkrishnamurthy@jumptrading.com>
2022-08-02 09:52:53 -0700

    stuff

--- program/rust/src/rust_oracle.rs
+++ program/rust/src/rust_oracle.rs
@@ -1,11 +1,14 @@
+use std::mem::{size_of, size_of_val};
+
 use solana_program::program_error::ProgramError;
-use super::c_entrypoint_wrapper;
-use crate::error::OracleResult;
+use solana_program::program_memory::sol_memset;
 use solana_program::pubkey::Pubkey;
 use solana_program::sysvar::slot_history::AccountInfo;
-use solana_program::program_memory::sol_memset;
-use crate::c_oracle_header::{pc_map_table_t, SUCCESS};
-use std::mem::{size_of, size_of_val};
+
+use crate::c_oracle_header::pc_map_table_t;
+use crate::error::OracleResult;
+
+use super::c_entrypoint_wrapper;
 
 ///Calls the c oracle update_price, and updates the Time Machine if needed
 pub fn update_price(
@@ -39,17 +42,17 @@ pub fn init_mapping(
     // FIXME: this is an extremely scary way to assert because if you forget the ? it doesn't do anything.
     pyth_assert(accounts.len() == 2 &&
                   valid_funding_account(accounts.get(0).unwrap()) &&
-                  valid_signable_account(program_id, accounts.get(1).unwrap(), sizeof( pc_map_table_t )),
-                ERROR_INVALID_ARGUMENT)?;
+                  valid_signable_account(program_id, accounts.get(1).unwrap(), size_of::<pc_map_table_t>),
+                ProgramError::InvalidArgument)?;
 
     let data = accounts.get(1)
       .unwrap()
       .try_borrow_data()
-      .map_err(|_| ERROR_INVALID_ARGUMENT)?;
+      .map_err(|_| ProgramError::InvalidArgument)?;
     let mapping_account = load::<pc_map_table_t>(*data)?;
 
     // Check that the account has not already been initialized
-    pyth_assert(mapping_account.magic_ == 0 && mapping_account.ver_ == 0, ERROR_INVALID_ARGUMENT)?;
+    pyth_assert(mapping_account.magic_ == 0 && mapping_account.ver_ == 0, ProgramError::InvalidArgument)?;
 
     // Initialize by setting to zero again (just in case) and setting
     // the version number
@@ -60,7 +63,8 @@ pub fn init_mapping(
     mapping_account.type_  = PC_ACCTYPE_MAPPING;
     mapping_account.size_  = size_of::<pc_map_table_t> - size_of_val( mapping_account.prod_ );
 
-    return Result::Ok(SUCCESS);
+    // TODO: bind SUCCESS
+    return Result::Ok(0);
 }
 
 pub fn pyth_assert(condition: bool, error_code: ProgramError) -> Result<(), ProgramError> {
