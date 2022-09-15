e859bc865931f8b2bf39b7a262a5e5d0fcf65bc2
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-09-14 20:32:25 -0500

    Test

---
+++ program/remote-governor/src/lib.rs
@@ -0,0 +1,7 @@
+#[allow(dead_code)]
+fn main() {
+    // Statements here are executed when the compiled binary is called
+    // solitaire(p, a, d);
+    // Print text to the console
+    println!("Hello World!");
+}
