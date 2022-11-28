262e6c999d81fec267883b7b61cbdc4d324b8183
guibescos <59208140+guibescos@users.noreply.github.com>
2022-11-28 19:57:27 +0000

    Remove build utils (#320)

--- program/rust/build.rs
+++ program/rust/build.rs
@@ -1,17 +1,12 @@
-mod build_utils;
 use bindgen::Builder;
 
 fn main() {
     println!("cargo:rustc-link-search=./program/c/target");
 
-    // Make a parser and to it type, traits pairs
-    let parser = build_utils::DeriveAdderParserCallback::new();
-
     // Generate and write bindings
     let bindings = Builder::default()
         .clang_arg("-I../../../solana/sdk/bpf/c/inc/")
         .header("./src/bindings.h")
-        .parse_callbacks(Box::new(parser))
         .rustfmt_bindings(true)
         .generate()
         .expect("Unable to generate bindings");
