eecef5adc4fa9a7459303194718b0882488e5ad2
Guillermo Bescos <gbescos@stanford.edu>
2022-11-29 17:38:23 +0000

    Fix size

---
+++ scripts/check-size.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# While Solana doesn't support resizing programs, the oracle binary needs to be smaller than 81760 bytes
+ORACLE_SIZE=$(wc -c ./target/deploy/pyth_oracle.so | awk '{print $1}')
+if [ $ORACLE_SIZE -lt 81760 ]
+then
+    echo "Size of pyth_oracle.so is small enough to be deployed to mainnet."
+    echo $ORACLE_SIZE
+else
+    echo "Size of pyth_oracle.so is too big to be deployed to mainnet."
+    echo $ORACLE_SIZE
+    exit 1
+fi
