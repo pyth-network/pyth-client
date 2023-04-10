91e30b01241fadd538d7cbb656e84226af3ad64e
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2023-04-10 10:49:24 -0700

    initial commit

--- program/rust/build.rs
+++ program/rust/build.rs
@@ -7,33 +7,35 @@ use {
 };
 
 fn main() {
-    // Cargo exposes ENV variables for feature flags that can be used to determine whether to do a
-    // self-contained build that compiles the C components automatically.
-    if std::env::var("CARGO_FEATURE_LIBRARY").is_ok() {
-        // OUT_DIR is the path cargo provides to a build directory under `target/` specifically for
-        // isolated build artifacts. We use this to build the C program and then link against the
-        // resulting static library. This allows building the program when used as a dependency of
-        // another crate.
-        let out_dir = std::env::var("OUT_DIR").unwrap();
-        let out_dir = PathBuf::from(out_dir);
+    // OUT_DIR is the path cargo provides to a build directory under `target/` specifically for
+    // isolated build artifacts. We use this to build the C program and then link against the
+    // resulting static library. This allows building the program when used as a dependency of
+    // another crate.
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let out_dir = PathBuf::from(out_dir);
 
-        // We must forward OUT_DIR as an env variable to the make script otherwise it will output
-        // its artifacts to the wrong place.
-        std::process::Command::new("make")
-            .env("VERBOSE", "1")
-            .env("OUT_DIR", out_dir.display().to_string())
-            .current_dir("../c")
-            .args(["cpyth-native"])
-            .status()
-            .expect("Failed to build C program");
+    // We must forward OUT_DIR as an env variable to the make script otherwise it will output
+    // its artifacts to the wrong place.
+    std::process::Command::new("make")
+        .env("VERBOSE", "1")
+        .env("OUT_DIR", out_dir.display().to_string())
+        .current_dir("../c")
+        .args(["cpyth-native", "cpyth-bpf"])
+        .status()
+        .expect("Failed to build C program");
 
-        // Emit instructions for cargo to link against the built static library.
-        println!("cargo:rustc-link-lib=static=cpyth-native");
-        println!("cargo:rustc-link-search={}", out_dir.display());
+    // Emit instructions for cargo to link against the built static library.
+
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    if target_arch == "bpf" {
+        // do something special for BPF target architecture
+        println!("cargo:rustc-link-lib=static=cpyth-bpf");
     } else {
-        // Otherwise fall back to the old relative-path C style build we used to have.
-        println!("cargo:rustc-link-search=./program/c/target");
+        // do something else for other target architectures
+        println!("cargo:rustc-link-lib=static=cpyth-native");
     }
+    println!("cargo:rustc-link-search={}", out_dir.display());
+
 
     // Generate and write bindings
     let bindings = Builder::default()
