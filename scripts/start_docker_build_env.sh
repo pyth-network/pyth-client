3afee1d6db42313a12d2e17f3a2ce7a9175e37bd
swimricky <ricky@swim.io>
2023-04-26 14:24:35 -0700

    chore: remove unnecessary mounts

--- scripts/start_docker_build_env.sh
+++ scripts/start_docker_build_env.sh
@@ -6,13 +6,11 @@ dir=$(pwd)
 parentdir="$(dirname "$dir")"
 export PYTH_REPO=$parentdir
 
-# Use the latest pythd image from https://hub.docker.com/r/pythfoundation/pyth-client/tags?name=pythd
-LATEST_VERSION="v2.17.3"
-export IMAGE=pythfoundation/pyth-client:pythd-${LATEST_VERSION}
+# Use the latest oracle image from https://hub.docker.com/r/pythfoundation/pyth-client/tags?name=oracle
+LATEST_VERSION="v2.21.0"
+export IMAGE=pythfoundation/pyth-client:oracle-${LATEST_VERSION}
 
 docker run -it \
-        --volume "${HOME}:/home/pyth/home" \
-        --volume "${HOME}/.config:/home/pyth/.config" \
         --mount "type=bind,src=${PYTH_REPO},target=/home/pyth/pyth-client" \
         --platform linux/amd64 \
         $IMAGE \
