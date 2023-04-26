d0ea71c60b84e8dc0ea728f96b866359192d70fa
Guillermo Bescos <g.bescos@yahoo.com>
2023-04-26 16:25:07 -0500

    Add check for the upper bound

--- program/rust/src/tests/test_sizes.rs
+++ program/rust/src/tests/test_sizes.rs
@@ -73,7 +73,8 @@ fn test_sizes() {
     assert_eq!(size_of::<PriceComponent>(), 96);
     assert_eq!(size_of::<PriceEma>(), 24);
     assert_eq!(size_of::<PriceAccount>(), 3312);
-    assert_eq!(PRICE_ACCOUNT_SIZE, 3312);
+    assert_eq!(PRICE_ACCOUNT_SIZE, 12570);
+    assert!(size_of::<PriceAccount>() <= try_convert::<_, usize>(PRICE_ACCOUNT_SIZE).unwrap());
     assert_eq!(size_of::<PermissionAccount>(), 12570);
 }
 
