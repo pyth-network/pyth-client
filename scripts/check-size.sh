7ed14c9f3eabf027a270984470f5a0bb531ebd88
Guillermo Bescos <g.bescos@yahoo.com>
2023-04-28 13:33:59 -0500

    Update size check to pythnet

--- scripts/check-size.sh
+++ scripts/check-size.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 #
+
 # While Solana doesn't support resizing programs, the oracle binary needs to be smaller than 81760 bytes
+# (The available space for the oracle program on pythnet is 88429 and mainnet is 81760)
 ORACLE_SIZE=$(wc -c ./target/deploy/pyth_oracle.so | awk '{print $1}')
-if [ $ORACLE_SIZE -lt 81760 ]
+if [ $ORACLE_SIZE -lt 88429 ]
 then
     echo "Size of pyth_oracle.so is small enough to be deployed to mainnet."
     echo $ORACLE_SIZE
