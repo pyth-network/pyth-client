dd415b795029abf27e51475f9e4ca710c45b39d6
Guillermo Bescos <g.bescos@yahoo.com>
2023-05-09 10:40:19 -0500

    Cleanup

--- program/rust/src/processor/upd_price.rs
+++ program/rust/src/processor/upd_price.rs
@@ -194,13 +194,13 @@ pub fn upd_price(
                     OracleError::InvalidPda.into(),
                 )?;
 
-                    // anchor discriminator for "global:put_all"
-                    let discriminator = [212, 225, 193, 91, 151, 238, 20, 93];
-                    let create_inputs_ix = Instruction::new_with_borsh(
-                        *accumulator_accounts.program_id.key,
-                        &(discriminator, price_account.key.to_bytes(), message),
-                        account_metas,
-                    );
+                // anchor discriminator for "global:put_all"
+                let discriminator = [212, 225, 193, 91, 151, 238, 20, 93];
+                let create_inputs_ix = Instruction::new_with_borsh(
+                    *accumulator_accounts.program_id.key,
+                    &(discriminator, price_account.key.to_bytes(), message),
+                    account_metas,
+                );
 
                 let message = vec![
                     PriceFeedMessage::from_price_account(price_account.key, &price_data)
