22ee4704d4e59d92d3225d333ef53661700c58e4
Stan Drozd <stan@pyth.network>
2023-10-03 16:06:04 +0200

    scripts: Move contract binaries to target/pyth/{solana|pythnet}
    This change attempts to provide both contract flavors as a result of
    the Docker build. Making both available will help with completing
    deployment to both environments.

--- scripts/check-size.sh
+++ scripts/check-size.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 #
+set -e
+
+max_size=$1
+filename=${2:-./target/deploy/pyth_oracle.so}
 
 # While Solana doesn't support resizing programs, the oracle binary needs to be smaller than 81760 bytes
 # (The available space for the oracle program on pythnet is 88429 and mainnet is 81760)
-ORACLE_SIZE=$(wc -c ./target/deploy/pyth_oracle.so | awk '{print $1}')
-if [ $ORACLE_SIZE -lt ${1} ]
+ORACLE_SIZE=$(wc -c $filename | awk '{print $1}')
+if [ $ORACLE_SIZE -lt ${max_size} ]
 then
-    echo "Size of pyth_oracle.so is small enough to be deployed, since ${ORACLE_SIZE} is less than ${1}"
+    echo "Size of ${filename} is small enough to be deployed, since ${ORACLE_SIZE} is less than ${max_size}"
 else
-    echo "Size of pyth_oracle.so is too big to be deployed, since ${ORACLE_SIZE} is greater than ${1}"
+    echo "Size of ${filename} is too big to be deployed, since ${ORACLE_SIZE} is greater than ${max_size}"
     exit 1
 fi
