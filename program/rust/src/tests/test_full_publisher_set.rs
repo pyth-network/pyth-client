1dab1fa4e5fc5ee05eee3fbc1b8fec44c257639e
Stan Drozd <stan@pyth.network>
2023-11-09 18:37:45 +0100

    test_full_publisher_set.rs: Use a 50/50 division instead of thirds

--- program/rust/src/tests/test_full_publisher_set.rs
+++ program/rust/src/tests/test_full_publisher_set.rs
@@ -4,7 +4,6 @@ use {
         c_oracle_header::{
             PC_NUM_COMP,
             PC_STATUS_TRADING,
-            PC_STATUS_UNKNOWN,
         },
         tests::pyth_simulator::{
             PythSimulator,
@@ -31,7 +30,7 @@ use {
 // different values, this time using mid_third and last_third without first_third
 // * Verify again for the new values meeting in the middle as aggregate.
 #[tokio::test]
-async fn test_full_publisher_set() -> Result<(), Box<dyn std::error::Error>> {
+async fn test_full_publisher_set_two_thirds() -> Result<(), Box<dyn std::error::Error>> {
     let mut sim = PythSimulator::new().await;
     let pub_keypairs: Vec<_> = (0..PC_NUM_COMP).map(|_idx| Keypair::new()).collect();
     let pub_pubkeys: Vec<_> = pub_keypairs.iter().map(|kp| kp.pubkey()).collect();
@@ -50,10 +49,9 @@ async fn test_full_publisher_set() -> Result<(), Box<dyn std::error::Error>> {
     // ensures that for Solana oracle, any two of the parts form a
     // subset that's larger than default min_pub_ value (currently
     // 20).
-    let (first_third, other_two_thirds) = pub_keypairs.split_at(n_pubs / 3 + 1);
-    let (mid_third, last_third) = other_two_thirds.split_at(n_pubs / 3 + 1);
+    let (first_half, second_half) = pub_keypairs.split_at(n_pubs / 2);
 
-    for (first_kp, mid_kp) in first_third.iter().zip(mid_third.iter()) {
+    for (idx, (first_kp, second_kp)) in first_half.iter().zip(second_half.iter()).enumerate() {
         let first_quote = Quote {
             price:      100,
             confidence: 30,
@@ -62,16 +60,20 @@ async fn test_full_publisher_set() -> Result<(), Box<dyn std::error::Error>> {
 
         sim.upd_price(first_kp, price, first_quote).await?;
 
-        let mid_quote = Quote {
+        let second_quote = Quote {
             price:      120,
             confidence: 30,
             status:     PC_STATUS_TRADING,
         };
 
-        sim.upd_price(mid_kp, price, mid_quote).await?;
+        sim.upd_price(second_kp, price, second_quote).await?;
 
-        slot += 1;
-        sim.warp_to_slot(slot).await?;
+
+        // Advance every 10th slot
+        if idx % 2 == 0 {
+            slot += 1;
+            sim.warp_to_slot(slot).await?;
+        }
     }
 
     {
@@ -84,50 +86,6 @@ async fn test_full_publisher_set() -> Result<(), Box<dyn std::error::Error>> {
         assert_eq!(price_data.agg_.conf_, 20);
     }
 
-    // Exclude first third from aggregation
-    for first_kp in first_third {
-        let first_quote = Quote {
-            price:      42,
-            confidence: 1,
-            status:     PC_STATUS_UNKNOWN,
-        };
-
-        sim.upd_price(first_kp, price, first_quote).await?;
-
-        slot += 1;
-        sim.warp_to_slot(slot).await?;
-    }
-
-    for (mid_kp, last_kp) in mid_third.iter().zip(last_third.iter()) {
-        let mid_quote = Quote {
-            price:      60,
-            confidence: 10,
-            status:     PC_STATUS_TRADING,
-        };
-
-        sim.upd_price(mid_kp, price, mid_quote).await?;
-
-        let last_quote = Quote {
-            price:      80,
-            confidence: 10,
-            status:     PC_STATUS_TRADING,
-        };
-
-        sim.upd_price(last_kp, price, last_quote).await?;
-
-        slot += 1;
-        sim.warp_to_slot(slot).await?;
-    }
-
-    {
-        let price_data = sim
-            .get_account_data_as::<PriceAccount>(price)
-            .await
-            .unwrap();
-
-        assert_eq!(price_data.agg_.price_, 70);
-        assert_eq!(price_data.agg_.conf_, 10);
-    }
 
     Ok(())
 }
