4d78c5b095ea09e02856b5b1ca421d3e71775bf3
Guillermo Bescos <g.bescos@yahoo.com>
2024-02-27 13:34:46 +0000

    This

--- program/rust/src/processor/set_max_latency.rs
+++ program/rust/src/processor/set_max_latency.rs
@@ -7,8 +7,8 @@ use {
         },
         instruction::SetMaxLatencyArgs,
         utils::{
+            check_permissioned_funding_account,
             check_valid_funding_account,
-            check_valid_signable_account_or_permissioned_funding_account,
             pyth_assert,
         },
         OracleError,
@@ -37,18 +37,17 @@ pub fn set_max_latency(
         ProgramError::InvalidArgument,
     )?;
 
-    let (funding_account, price_account, permissions_account_option) = match accounts {
-        [x, y] => Ok((x, y, None)),
-        [x, y, p] => Ok((x, y, Some(p))),
+    let (funding_account, price_account, permissions_account) = match accounts {
+        [x, y, p] => Ok((x, y, p)),
         _ => Err(OracleError::InvalidNumberOfAccounts),
     }?;
 
     check_valid_funding_account(funding_account)?;
-    check_valid_signable_account_or_permissioned_funding_account(
+    check_permissioned_funding_account(
         program_id,
         price_account,
         funding_account,
-        permissions_account_option,
+        permissions_account,
         &cmd.header,
     )?;
 
