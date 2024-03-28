05b55194ac3611d4e292570a3fe333faa19ea130
Daniel Chew <cctdaniel@outlook.com>
2024-03-28 14:50:00 +0900

    update c format

--- program/c/src/oracle/upd_aggregate.c
+++ program/c/src/oracle/upd_aggregate.c
@@ -1,16 +1,18 @@
 /*
  * BPF upd_aggregate binding
  */
-#include <solana_sdk.h>
-#include "oracle.h"
 #include "upd_aggregate.h"
 #include "features.h"
+#include "oracle.h"
+#include <solana_sdk.h>
 
-
-extern bool c_upd_aggregate_pythnet( pc_price_t *ptr, uint64_t slot, int64_t timestamp ){
-  return upd_aggregate(ptr, slot, timestamp );
+extern bool c_upd_aggregate_pythnet(pc_price_t *ptr, uint64_t slot,
+                                    int64_t timestamp)
+{
+  return upd_aggregate(ptr, slot, timestamp);
 }
 
-extern void c_upd_twap( pc_price_t *ptr, int64_t nslots ){
+extern void c_upd_twap(pc_price_t *ptr, int64_t nslots)
+{
   upd_twap(ptr, nslots);
 }
