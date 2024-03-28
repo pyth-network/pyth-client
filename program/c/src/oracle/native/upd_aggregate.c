5817b1d88c6e99e6d10271442ba9e876401d7c5e
Daniel Chew <cctdaniel@outlook.com>
2024-03-28 14:55:20 +0900

    revert format

--- program/c/src/oracle/native/upd_aggregate.c
+++ program/c/src/oracle/native/upd_aggregate.c
@@ -1,9 +1,7 @@
-/// The goal of this file is to provide the upd_aggregate function to local rust
-/// tests
+/// The goal of this file is to provide the upd_aggregate function to local rust tests
 
 /// We need to allocate some heap space for upd_aggregate
-/// When compiling for the solana runtime, the heap space is preallocated and
-/// PC_HEAP_START is provided by <solana.h>
+/// When compiling for the solana runtime, the heap space is preallocated and PC_HEAP_START is provided by <solana.h>
 char heap_start[8192];
 #define PC_HEAP_START (heap_start)
 #define static_assert _Static_assert
@@ -11,13 +9,10 @@ char heap_start[8192];
 #include "../upd_aggregate.h"
 #include "../features.h"
 
-extern bool c_upd_aggregate_pythnet(pc_price_t *ptr, uint64_t slot,
-                                    int64_t timestamp)
-{
-  return upd_aggregate(ptr, slot, timestamp);
+extern bool c_upd_aggregate_pythnet( pc_price_t *ptr, uint64_t slot, int64_t timestamp ){
+  return upd_aggregate(ptr, slot, timestamp );
 }
 
-extern void c_upd_twap(pc_price_t *ptr, int64_t nslots)
-{
+extern void c_upd_twap( pc_price_t *ptr, int64_t nslots ){
   upd_twap(ptr, nslots);
 }
