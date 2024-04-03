f2c96f009df514c7906123df22dd62453bd63554
Daniel Chew <cctdaniel@outlook.com>
2024-04-03 13:43:46 +0900

    add back deleted test in test_publish

--- program/rust/src/tests/test_publish.rs
+++ program/rust/src/tests/test_publish.rs
@@ -64,6 +64,20 @@ async fn test_publish() {
     .await
     .unwrap();
 
+    {
+        let price_data = sim
+            .get_account_data_as::<PriceAccount>(price)
+            .await
+            .unwrap();
+
+        assert_eq!(price_data.comp_[0].latest_.price_, 150);
+        assert_eq!(price_data.comp_[0].latest_.conf_, 7);
+        assert_eq!(price_data.comp_[0].latest_.status_, PC_STATUS_TRADING);
+        assert_eq!(price_data.comp_[0].agg_.price_, 150);
+        assert_eq!(price_data.comp_[0].agg_.conf_, 7);
+        assert_eq!(price_data.comp_[0].agg_.status_, PC_STATUS_TRADING);
+    }
+
     sim.warp_to_slot(2).await.unwrap();
     sim.upd_price(
         &publisher,
