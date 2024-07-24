0783aa5af8d551ab13693b50957a54b934955b72
guibescos <59208140+guibescos@users.noreply.github.com>
2024-07-24 22:46:12 +0100

    Update program/rust/src/accounts/price.rs
    Co-authored-by: Pavel Strakhov <ri@idzaaus.org>

--- program/rust/src/accounts/price.rs
+++ program/rust/src/accounts/price.rs
@@ -348,7 +348,7 @@ impl PythOracleSerialize for PublisherStakeCapsMessage {
         let mut result = vec![DISCRIMINATOR];
         result.extend_from_slice(&self.publish_time.to_be_bytes());
         result.extend_from_slice(
-            &TryInto::<u16>::try_into(self.caps.as_ref().len())
+            &u16::try_from(self.caps.as_ref().len())
                 .unwrap()
                 .to_be_bytes(),
         );
